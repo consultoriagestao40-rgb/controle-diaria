237d6b731ff982b3c95e71e30a3d2c3293d4028a
Cristiano Silva <cristiano.godoi@gotmail.com>
2026-01-08 22:53:56 -0300

    Fix: Remove options from z.date to fix build

--- src/app/dashboard/supervisor/nova/page.tsx
+++ src/app/dashboard/supervisor/nova/page.tsx
@@ -48,9 +48,7 @@ import Link from "next/link"
 
 // Validation Schema
 const formSchema = z.object({
-    data: z.date({
-        invalid_type_error: "Data é obrigatória.",
-    }),
+    data: z.date(),
     postoId: z.string().min(1, "Posto é obrigatório."),
     diaristaId: z.string().min(1, "Diarista é obrigatória."),
     reservaId: z.string().min(1, "Colaborador é obrigatório."),
